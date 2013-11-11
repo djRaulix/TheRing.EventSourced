83924a607667b3deb1a60aa903078c0190197514
juicelink <chezju@gmail.com>
2013-11-11 12:45:33 +0100

    corrections on repository

---
+++ TheRing.EventSourced.Domain/Repository/Create/AggregateRootFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace TheRing.EventSourced.Domain.Repository.Create
+{
+    using TheRing.EventSourced.Core.Reflection.Activator;
+    using TheRing.EventSourced.Domain.Aggregate;
+
+    public class AggregateRootFactory : IAggregateRootFactory
+    {
+        public T New<T>() where T : AggregateRoot
+        {
+            return Activator<T>.CreateInstance();
+        }
+    }
+}
--- TheRing.EventSourced.Domain/Repository/Get/AggregateRootGetter.cs
+++ TheRing.EventSourced.Domain/Repository/Get/AggregateRootGetter.cs
@@ -4,9 +4,7 @@
 
     using System;
 
-    using TheRing.EventSourced.Core;
     using TheRing.EventSourced.Domain.Aggregate;
-    using TheRing.EventSourced.Domain.Exceptions;
     using TheRing.EventSourced.Domain.Repository.Create;
 
     #endregion
@@ -17,22 +15,13 @@
 
         private readonly ICreateAggregateRoot aggregateRootCreator;
 
-        private readonly IGetEventsOnStream eventsGetter;
-
-        private readonly IGetStreamNameFromAggregateRoot streamNameGetter;
-
         #endregion
 
         #region Constructors and Destructors
 
-        public AggregateRootGetter(
-            ICreateAggregateRoot aggregateRootCreator, 
-            IGetEventsOnStream eventsGetter, 
-            IGetStreamNameFromAggregateRoot streamNameGetter)
+        public AggregateRootGetter(ICreateAggregateRoot aggregateRootCreator)
         {
             this.aggregateRootCreator = aggregateRootCreator;
-            this.eventsGetter = eventsGetter;
-            this.streamNameGetter = streamNameGetter;
         }
 
         #endregion
@@ -42,14 +31,6 @@
         public TAgg Get<TAgg>(Guid id) where TAgg : AggregateRoot
         {
             var aggregateRoot = this.aggregateRootCreator.Create<TAgg>(id);
-
-            aggregateRoot.LoadFromHistory(
-                this.eventsGetter.Get(this.streamNameGetter.Get(aggregateRoot), aggregateRoot.Version + 1));
-
-            if (aggregateRoot.Version == 0)
-            {
-                throw new UnKnownAggregateRootException(id);
-            }
 
             return aggregateRoot;
         }
