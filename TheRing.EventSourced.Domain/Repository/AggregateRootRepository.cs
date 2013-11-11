d2f5a612747ed5a8cfcab5bf2541a60c2b624d6d
juicelink <chezju@gmail.com>
2013-11-11 02:52:53 +0100

    split repository file

---
+++ TheRing.EventSourced.Domain/Repository/AggregateRootRepository.cs
@@ -0,0 +1,52 @@
+﻿namespace TheRing.EventSourced.Domain.Repository
+{
+    using System;
+
+    using TheRing.EventSourced.Domain.Aggregate;
+    using TheRing.EventSourced.Domain.Repository.Create;
+    using TheRing.EventSourced.Domain.Repository.Get;
+    using TheRing.EventSourced.Domain.Repository.Save;
+
+    public class AggregateRootRepository : IAggregateRootRepository
+    {
+        #region Fields
+
+        private readonly ICreateAggregateRoot creator;
+
+        private readonly IGetAggregateRoot getter;
+
+        private readonly ISaveAggregateRoot saver;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public AggregateRootRepository(ICreateAggregateRoot creator, IGetAggregateRoot getter, ISaveAggregateRoot saver)
+        {
+            this.creator = creator;
+            this.getter = getter;
+            this.saver = saver;
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public TAgg Create<TAgg>(Guid id) where TAgg : AggregateRoot
+        {
+            return this.creator.Create<TAgg>(id);
+        }
+
+        public TAgg Get<TAgg>(Guid id) where TAgg : AggregateRoot
+        {
+            return this.getter.Get<TAgg>(id);
+        }
+
+        public void Save<TAgg>(TAgg aggregateRoot) where TAgg : AggregateRoot
+        {
+            this.saver.Save(aggregateRoot);
+        }
+
+        #endregion
+    }
+}
--- TheRing.EventSourced.Domain/Repository/IAggregateRootRepository.cs
+++ TheRing.EventSourced.Domain/Repository/IAggregateRootRepository.cs
@@ -5,9 +5,6 @@
     using System;
 
     using TheRing.EventSourced.Domain.Aggregate;
-    using TheRing.EventSourced.Domain.Repository.Create;
-    using TheRing.EventSourced.Domain.Repository.Get;
-    using TheRing.EventSourced.Domain.Repository.Save;
 
     #endregion
 
@@ -20,49 +17,6 @@
         TAgg Get<TAgg>(Guid id) where TAgg : AggregateRoot;
 
         void Save<TAgg>(TAgg aggregateRoot) where TAgg : AggregateRoot;
-
-        #endregion
-    }
-
-    public class AggregateRootRepository : IAggregateRootRepository
-    {
-        #region Fields
-
-        private readonly ICreateAggregateRoot creator;
-
-        private readonly IGetAggregateRoot getter;
-
-        private readonly ISaveAggregateRoot saver;
-
-        #endregion
-
-        #region Constructors and Destructors
-
-        public AggregateRootRepository(ICreateAggregateRoot creator, IGetAggregateRoot getter, ISaveAggregateRoot saver)
-        {
-            this.creator = creator;
-            this.getter = getter;
-            this.saver = saver;
-        }
-
-        #endregion
-
-        #region Public Methods and Operators
-
-        public TAgg Create<TAgg>(Guid id) where TAgg : AggregateRoot
-        {
-            return this.creator.Create<TAgg>(id);
-        }
-
-        public TAgg Get<TAgg>(Guid id) where TAgg : AggregateRoot
-        {
-            return this.getter.Get<TAgg>(id);
-        }
-
-        public void Save<TAgg>(TAgg aggregateRoot) where TAgg : AggregateRoot
-        {
-            this.saver.Save(aggregateRoot);
-        }
 
         #endregion
     }
