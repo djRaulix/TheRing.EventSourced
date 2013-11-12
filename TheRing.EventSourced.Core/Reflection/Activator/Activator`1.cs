bebbefc5f5099383677776f4fb748fa468b3e590
juicelink <chezju@gmail.com>
2013-11-12 22:35:22 +0100

    add agg factory

---
+++ TheRing.EventSourced.Core/Reflection/Activator/Activator`1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheRing.EventSourced.Core.Reflection.Activator
+{
+    public static class Activator<T> where T : class
+    {
+        public static Func<T> CreateInstance { get; private set; }
+
+        static Activator()
+        {
+            CreateInstance = typeof(T).GetConstructorDelegate<T>();
+        }
+    }
+}
---
+++ TheRing.EventSourced.Core/Reflection/Activator/Activator`2.cs
@@ -0,0 +1,14 @@
+﻿namespace TheRing.EventSourced.Core.Reflection.Activator
+{
+    using System;
+
+    public static class Activator<T, TArg1> where T : class
+    {
+        public static Func<TArg1, T> CreateInstance { get; private set; }
+
+        static Activator()
+        {
+            CreateInstance = (Func<TArg1, T>)typeof(T).GetConstructorDelegate(typeof(Func<TArg1, T>));
+        }
+    }
+}
