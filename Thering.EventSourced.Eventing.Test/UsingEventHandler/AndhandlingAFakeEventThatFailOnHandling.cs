ed2e4b93ce696e5a83e934401cb0245acaf38610
djRaulix <j.dumasderauly@gmail.com>
2014-01-30 00:10:53 +0100

    Reorganize EventHandler Test

---
+++ Thering.EventSourced.Eventing.Test/UsingEventHandler/AndhandlingAFakeEventThatFailOnHandling.cs
@@ -0,0 +1,33 @@
+﻿namespace Thering.EventSourced.Eventing.Test.UsingEventHandler
+{
+    using System;
+
+    using FakeItEasy;
+    using FakeItEasy.ExtensionSyntax.Full;
+
+    using NUnit.Framework;
+
+    using TheRing.Test.Fakes;
+
+    public class AndhandlingAFakeEventThatFailOnHandling : UsingEventHandler
+    {
+        private readonly Guid eventId = Guid.NewGuid();
+       
+
+        protected override void BecauseOf()
+        {
+            base.BecauseOf();
+            this.EventHandler.Handle(new FakeEvent(this.eventId));
+        }
+
+        [Test]
+        public void ThenEventShouldBeHandleByErrorHandler()
+        {
+            var thrownErrorMessage = ((FakeEventHandlerThatFail)this.FakeEventHandler).ThrownException.Message;
+
+            this.ErrorHandler.CallsTo(handler => 
+                handler.HandleError(A<FakeEvent>.That.Matches(e => e.No == this.eventId), A<Exception>.That.Matches(e => e.Message.Equals(thrownErrorMessage))))
+                             .MustHaveHappened(Repeated.Exactly.Once);
+        }
+    }
+}
---
+++ Thering.EventSourced.Eventing.Test/UsingEventHandler/UsingEventHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Thering.EventSourced.Eventing.Test.UsingEventHandler
+{
+    using FakeItEasy;
+
+    using TheRing.Test;
+
+    public abstract class UsingEventHandler : SpecBase
+    {
+        private readonly FakeEventHandlerThatFail fakeEventHandler = new FakeEventHandlerThatFail();
+
+        protected FakeEventHandler FakeEventHandler
+        {
+            get
+            {
+                return this.fakeEventHandler;
+            }
+        }
+
+        protected IHandleError ErrorHandler { get; private set; }
+
+        protected EventHandler EventHandler { get; private set; }
+
+        protected override void EstablishContext()
+        {
+            base.EstablishContext();
+            this.ErrorHandler = A.Fake<IHandleError>();
+            this.EventHandler = new EventHandler(this.FakeEventHandler, this.ErrorHandler);
+        }
+    }
+}
